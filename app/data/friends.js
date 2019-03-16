0ae56ac2d03fa3be55987c9259ed82efc11261cf
Aaron Smith <no-reply>
2019-03-15 23:14:38 -0500

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [{
+        "name": "Beyoncé",
+        "photo": "https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/b/2/e/1/b2e163304f3dfdbae3b25fa4a77c58dd.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        "name": "Taylor Swift",
+        "photo": "https://akns-images.eonline.com/eol_images/Entire_Site/2017107/rs_1080x1920-171107070604-1080.taylor-swift.11717.jpg?fit=inside|900:auto&output-quality=90",
+        "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        "name": "Jennifer Aniston",
+        "photo": "https://s3.r29static.com//bin/entry/a87/720x864,85/2087892/image.webp",
+        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        "name": "Megan Fox",
+        "photo": "https://static.tvtropes.org/pmwiki/pub/images/rsz_meganfox.png",
+        "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        "name": "Kim Kardashian",
+        "photo": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2018%2F10%2Fkim-kardashian2.jpg%3Fcrop%3D0px%252C0px%252C1000px%252C1000px%26resize%3D1000%252C1000&w=380&h=380&c=sc&poi=face&q=85",
+        "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        "name": "Reese Witherspoon",
+        "photo": "https://vg-images.condecdn.net/image/MMVyP7AlMlk/crop/405/f/reese-witherspoon-vogue-9feb15-getty_b.jpg",
+        "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        "name": "Jennifer Lawrence",
+        "photo": "http://photos.vanityfair.com/2014/10/06/5432e75e74ed87dd423a383b_jennifer-lawrence-vanity-fair-11-november-cover-ss03.jpg",
+        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    },
+    {
+        "name": "Angelina Jolie",
+        "photo": "https://m.media-amazon.com/images/M/MV5BODg3MzYwMjE4N15BMl5BanBnXkFtZTcwMjU5NzAzNw@@._V1_UY317_CR22,0,214,317_AL_.jpg",
+        "scores": [3, 5, 1, 4, 2, 2, 2, 4, 2, 3]
+    },
+    {
+        "name": "Eva Longoria",
+        "photo": "https://s.hdnux.com/photos/74/17/05/15790434/5/920x920.jpg",
+        "scores": [2, 3, 4, 1, 3, 2, 3, 4, 2, 5]
+    },
+    {
+        "name": "Sofía Vergara",
+        "photo": "https://www.maxim.com/.image/ar_8:10%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTQxNTM4NzQzNDk1NDM2MDI2/facebook-linked_image___mtm1mtq0mtk0ntkwmjgzmdi2jpg.jpg",
+        "scores": [3, 4, 2, 1, 2, 3, 4, 4, 4, 3]
+    }
+
+]
+
+module.exports = friends;
